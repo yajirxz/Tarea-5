1fafdd3ba87018e43723594b06541665d264c391
Usuario-PC <yahirroza594@outlook.com>
2023-03-28 20:46:37 -0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        MaquinaCafe maquina = new MaquinaCafe();
+
+        maquina.hacerAmericano();
+        maquina.hacerCapuchino();
+        maquina.hacerExpreso();
+        maquina.hacerAmericano();
+        maquina.hacerCapuchino();
+        maquina.hacerExpreso();
+        maquina.hacerAmericano();
+        maquina.hacerCapuchino();
+        maquina.hacerExpreso();
+        maquina.hacerAmericano();
+        maquina.hacerCapuchino();
+        maquina.hacerExpreso();
+        maquina.hacerExpreso();
+    }
+
 }
--- src/MaquinaCafe.java
+++ src/MaquinaCafe.java
@@ -1,89 +1,68 @@
 public class MaquinaCafe {
-    private int agua;
-    private int cafe;
-    private int crema;
-    private int vasos;
 
-    public MaquinaCafe() {
-        this.agua = 5000;
-        this.cafe = 1000;
-        this.crema = 1500;
-        this.vasos = 50;
-    }
+        private static final int MAX_AGUA = 5000; // mililitros
+        private static final int MAX_CAFE = 1000; // gramos
+        private static final int MAX_CREMA = 1500; // mililitros
+        private static final int MAX_VASOS = 50; // vasos
+        private static final int CAPACIDAD_VASO = 200; // mililitros
 
-    public void hacerAmericano() {
-        if (this.agua >= 180 && this.cafe >= 15 && this.vasos > 0) {
-            this.agua -= 180;
-            this.cafe -= 15;
-            this.vasos--;
-            System.out.println("Se ha servido un café americano.");
-            this.mostrarEstado();
-        } else {
-            System.out.println("No hay suficientes recursos para hacer un café americano.");
+        private int agua;
+        private int cafe;
+        private int crema;
+        private int vasos;
+
+        public MaquinaCafe() {
+            this.agua = MAX_AGUA;
+            this.cafe = MAX_CAFE;
+            this.crema = MAX_CREMA;
+            this.vasos = MAX_VASOS;
         }
-    }
 
-    public void hacerExpreso() {
-        if (this.agua >= 120 && this.cafe >= 20 && this.vasos > 0) {
-            this.agua -= 120;
-            this.cafe -= 20;
-            this.vasos--;
-            System.out.println("Se ha servido un café expreso.");
-            this.mostrarEstado();
-        } else {
-            System.out.println("No hay suficientes recursos para hacer un café expreso.");
+        public void hacerAmericano() {
+            if (agua >= 180 && cafe >= 15 && vasos > 0) {
+                agua -= 180;
+                cafe -= 15;
+                vasos--;
+                System.out.println("Café americano servido.");
+                mostrarEstado();
+            } else {
+                System.out.println("Lo siento, no hay suficientes recursos para hacer un café americano.");
+            }
         }
-    }
 
-    public void hacerCapuchino() {
-        if (this.agua >= 100 && this.cafe >= 14 && this.crema >= 70 && this.vasos > 0) {
-            this.agua -= 100;
-            this.cafe -= 14;
-            this.crema -= 70;
-            this.vasos--;
-            System.out.println("Se ha servido un capuchino.");
-            this.mostrarEstado();
-        } else {
-            System.out.println("No hay suficientes recursos para hacer un capuchino.");
+        public void hacerExpreso() {
+            if (agua >= 120 && cafe >= 20 && vasos > 0) {
+                agua -= 120;
+                cafe -= 20;
+                vasos--;
+                System.out.println("Café expreso servido.");
+                mostrarEstado();
+            } else {
+                System.out.println("Lo siento, no hay suficientes recursos para hacer un café expreso.");
+            }
         }
-    }
 
-    public void mostrarEstado() {
-        System.out.println("Estado de la máquina de café:");
-        System.out.println("- Agua: " + this.agua + " ml");
-        System.out.println("- Café: " + this.cafe + " g");
-        System.out.println("- Crema: " + this.crema + " ml");
-        System.out.println("- Vasos: " + this.vasos);
-    }
+        public void hacerCapuchino() {
+            if (agua >= 100 && crema >= 70 && cafe >= 14 && vasos > 0) {
+                agua -= 100;
+                crema -= 70;
+                cafe -= 14;
+                vasos--;
+                System.out.println("Capuchino servido.");
+                mostrarEstado();
+            } else {
+                System.out.println("Lo siento, no hay suficientes recursos para hacer un capuchino.");
+            }
+        }
 
-    public static void main(String[] args) {
-        MaquinaCafe maquina = new MaquinaCafe();
-
-        maquina.mostrarEstado();
-
-        maquina.hacerAmericano();
-        maquina.hacerAmericano();
-        maquina.hacerAmericano();
-        maquina.hacerExpreso();
-        maquina.hacerCapuchino();
-        maquina.hacerAmericano();
-        maquina.hacerExpreso();
-        maquina.hacerCapuchino();
-        maquina.hacerCapuchino();
-        maquina.hacerCapuchino();
-        maquina.hacerAmericano();
-        maquina.hacerAmericano();
-        maquina.hacerExpreso();
-        maquina.hacerExpreso();
-        maquina.hacerCapuchino();
-        maquina.hacerAmericano();
-        maquina.hacerCapuchino();
-        maquina.hacerExpreso();
-        maquina.hacerCapuchino();
-        maquina.hacerCapuchino();
-
-        maquina.mostrarEstado();
-    }
+        public void mostrarEstado() {
+            System.out.println("Agua: " + agua + " ml");
+            System.out.println("Café: " + cafe + " g");
+            System.out.println("Crema: " + crema + " ml");
+            System.out.println("Vasos: " + vasos);
+        }
 }
+
+
 
 
